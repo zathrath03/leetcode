6cc92f69475a1d44283c7b19e410c3acee7807a5
John <zathrath03@gmail.com>
2022-07-23 10:26:58 -0700

    Time: 113 ms (94.54%), Space: 14.2 MB (88.25%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,7 +1,12 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         output = []
-        strength = list(map(sum, mat))
+        strength = []
+        for row in mat:
+            try:
+                strength.append(row.index(0))
+            except ValueError:
+                strength.append(len(row))
         for _ in range(k):
             minIndex = strength.index(min(strength))
             output.append(minIndex)
