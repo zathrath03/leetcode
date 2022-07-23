11d6b154cb6deb344fef1ba447755942594442d4
John <zathrath03@gmail.com>
2022-07-22 22:28:36 -0700

    Time: 93 ms (48.02%), Space: 14 MB (94.04%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomDict = {}
+        magDict = {}
+        for char in ransomNote:
+            ransomDict[char] = ransomDict.get(char, 0) + 1
+        for char in magazine:
+            magDict[char] = magDict.get(char, 0) + 1
+        for key in ransomDict.keys():
+            if ransomDict[key] > magDict.get(key, 0):
+                return False
+        return True
