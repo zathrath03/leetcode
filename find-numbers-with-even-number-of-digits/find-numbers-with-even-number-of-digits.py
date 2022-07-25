7ba42d2a93e5c6f4d4908c7b148116c8c385d269
John <zathrath03@gmail.com>
2022-07-25 15:36:58 -0700

    Delete find-numbers-with-even-number-of-digits directory

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findNumbers(self, nums: List[int]) -> int:
-        count = 0
-        for num in nums:
-            if len("%i" % num) % 2 == 0:
-                count += 1
-        return count
-    
