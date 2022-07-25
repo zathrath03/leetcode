8774986dfed2507e0da1a7d95ab8989f39971f7b
John <zathrath03@gmail.com>
2022-07-25 14:29:18 -0700

    Time: 180 ms (5.42%), Space: 24.6 MB (16.53%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -4,22 +4,10 @@ class Solution:
             return ["1"]
         elif n % 3 == 0:
             if n % 5 == 0:
-                temp = self.fizzBuzz(n-1)
-                temp.append("FizzBuzz")
-                return temp
-                # return self.fizzBuzz(n-1).append("FizzBuzz")
+                return self.fizzBuzz(n-1) + ["FizzBuzz"]
             else:
-                temp = self.fizzBuzz(n-1)
-                temp.append("Fizz")
-                return temp
-                # return self.fizzBuzz(n-1).append("FizzBuzz")
+                return self.fizzBuzz(n-1) + ["Fizz"]
         elif n % 5 == 0:
-            temp = self.fizzBuzz(n-1)
-            temp.append("Buzz")
-            return temp
-            # return self.fizzBuzz(n-1).append("Buzz")
+            return self.fizzBuzz(n-1) + ["Buzz"]
         else:
-            temp = self.fizzBuzz(n-1)
-            temp.append(str(n))
-            return temp
-            # return self.fizzBuzz(n-1).append(str(n))
+            return self.fizzBuzz(n-1) + [str(n)]
