a5c8e246eeddbf5eaf1b587d4e19a008fdcfeac0
John <zathrath03@gmail.com>
2022-08-15 16:00:39 -0700

    Time: 124 ms (43.64%), Space: 14.8 MB (67.13%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -6,8 +6,8 @@ class Solution:
         i = 0
         while i < len(arr):
             if arr[i] == 0:
-                arr.insert(i, 0)
                 del arr[-1]
+                arr.insert(i, 0)                
                 i += 2
             else:
                 i += 1
