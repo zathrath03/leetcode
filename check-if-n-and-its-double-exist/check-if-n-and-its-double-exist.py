6fbd44a0a2e49872ddef4e71bbf2af49adfa9ccc
John <zathrath03@gmail.com>
2022-08-23 06:08:19 -0700

    Delete check-if-n-and-its-double-exist directory

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def checkIfExist(self, arr: List[int]) -> bool:
-        arr_set = set()
-        for val in arr:
-            if 2*val in arr_set or val/2 in arr_set:
-                return True
-            else:
-                arr_set.add(val)
-        return False
