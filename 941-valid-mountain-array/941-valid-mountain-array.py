1fe49fd5f54f42161f667823bab92c69e4416b73
John <zathrath03@gmail.com>
2022-08-23 06:23:11 -0700

    Time: 362 ms (33.41%), Space: 15.3 MB (97.00%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -5,13 +5,13 @@ class Solution:
 
         i = 1
         while arr[i] > arr[i-1]:
-            if i == len(arr) - 1:
+            if i == len(arr)-1:
                 return False
             i += 1
         while arr[i] < arr[i-1]:
             if i == 1:
                 return False
-            elif i == len(arr) - 1:
+            elif i == len(arr)-1:
                 return True
             i += 1
         return False
