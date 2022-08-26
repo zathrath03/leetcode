6c6cad01f1765d42bc4638a6841f7cd3110c9d8a
John <zathrath03@gmail.com>
2022-08-26 13:33:41 -0700

    Time: 53 ms (60.91%), Space: 13.8 MB (56.74%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,7 +1,6 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        sortedCopyOfHeights = heights.copy()
-        sortedCopyOfHeights.sort()
+        sortedCopyOfHeights = sorted(heights)
         count = 0
         
         for i in range(len(heights)):
