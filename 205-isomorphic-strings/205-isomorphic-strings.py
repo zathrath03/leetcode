4a4db38f12c6dfec62b28775e9cf216af19a3ee4
John <zathrath03@gmail.com>
2022-08-29 08:00:48 -0700

    Time: 43 ms (91.72%), Space: 14.2 MB (88.79%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,10 +1,3 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        charMapStoT = {}
-        charMapTtoS = {}
-        for i, char in enumerate(s):
-            if charMapStoT.setdefault(char, t[i]) != t[i]:
-                return False
-            if charMapTtoS.setdefault(t[i], char) != char:
-                return False
-        return True
+        return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
