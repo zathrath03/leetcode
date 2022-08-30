842b7bc5ebae271d8f414234f34c1432f0544739
John <zathrath03@gmail.com>
2022-08-30 14:38:56 -0700

    Time: 39 ms (92.67%), Space: 13.9 MB (79.33%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,35 +5,21 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Catch edge cases of empty lists
-        if not list1:
-            return list2
-        if not list2:
-            return list1
+        mergedListHead = ListNode()
+        mergedListTail = mergedListHead
         
-        # Initialize the mergedList with the smaller first value
-        if list1.val < list2.val:
-            mergedListHead = mergedList = ListNode(list1.val)
-            list1 = list1.next                
-        else:
-            mergedListHead = mergedList = ListNode(list2.val)
-            list2 = list2.next
-        
-        # Move through each list grabbing the smallest available
         while list1 and list2:
             if list1.val < list2.val:
-                mergedList.next = ListNode(list1.val)
-                mergedList = mergedList.next
+                mergedListTail.next = list1
                 list1 = list1.next
             else:
-                mergedList.next = ListNode(list2.val)
-                mergedList = mergedList.next
+                mergedListTail.next = list2
                 list2 = list2.next
+            mergedListTail = mergedListTail.next
         
-        # Attach the remaining list to the end of the merged list
         if list1:
-            mergedList.next = list1
+            mergedListTail.next = list1
         else:
-            mergedList.next = list2
-            
-        return mergedListHead
+            mergedListTail.next = list2
+        
+        return mergedListHead.next
