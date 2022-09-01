3b0742b123bfc5aab41dc8caa719f469c7414c0d
John <zathrath03@gmail.com>
2022-09-01 16:15:52 -0700

    Time: 70 ms (10.84%), Space: 13.9 MB (66.43%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,15 +1,9 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        charFrequency = {}
         length = 0
         oddPresent = False
         
-        for char in s:
-            charFrequency[char] = charFrequency.get(char, 0) + 1
-        
-        frequencies = charFrequency.values()
-        
-        for frequency in frequencies:
+        for frequency in collections.Counter(s).values():
             length += frequency // 2 * 2
             if not oddPresent and frequency % 2 == 1:
                 oddPresent = True
