57ea35a9bf82f4df80e7e0575533d022ba4e6112
John <zathrath03@gmail.com>
2022-09-03 11:27:06 -0700

    Time: 92 ms (29.76%), Space: 16.3 MB (17.35%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -1,14 +1,9 @@
 class Solution:   
-    def preorder(self, root: 'Node') -> List[int]:                
-        if not root:
-            return []
-        
-        preorder = []        
-        stack = [root]
-                    
-        while stack:
-            root = stack.pop()
-            preorder.append(root.val)
-            stack.extend(root.children[::-1])           
-        
-        return preorder
+    def preorder(self, root: 'Node') -> List[int]: 
+        def solve(root, ans):
+            if root:
+                ans.append(root.val)
+                for child in root.children:
+                    solve(child, ans)
+            return ans
+        return solve(root, [])
