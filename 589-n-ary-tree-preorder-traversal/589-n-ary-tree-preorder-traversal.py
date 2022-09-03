6a1fe05736a8bf093c7c959b485923d34dd15005
John <zathrath03@gmail.com>
2022-09-03 10:41:30 -0700

    Time: 78 ms (54.53%), Space: 16 MB (81.53%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -1,11 +1,3 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
-"""
-
 class Solution:   
     def preorder(self, root: 'Node') -> List[int]:                
         if not root:
@@ -17,7 +9,6 @@ class Solution:
         while stack:
             root = stack.pop()
             preorder.append(root.val)
-            for i in range(len(root.children)-1, -1, -1):
-                stack.append(root.children[i])            
+            stack.extend(root.children[::-1])           
         
         return preorder
