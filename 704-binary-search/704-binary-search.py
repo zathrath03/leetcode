d4bf3ae60af326e61710707ddc138595c40905bd
John <zathrath03@gmail.com>
2022-09-03 14:06:48 -0700

    Time: 333 ms (64.24%), Space: 15.4 MB (97.30%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        
+        index = len(nums) // 2
+        
+        if nums[index] == target:
+            return index
+        if nums[index] > target:
+            return self.search(nums[:index], target)
+        if nums[index] < target:
+            rtn = self.search(nums[index+1:], target)
+            if rtn == -1:
+                return rtn
+            else:
+                return rtn + index + 1
