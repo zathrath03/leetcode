43c8a4867c744db697ff990b54bbc57ae1612c03
John <zathrath03@gmail.com>
2022-09-04 16:01:08 -0700

    Time: 151 ms (20.08%), Space: 18.7 MB (68.21%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,20 +1,13 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        def contains(node, target):
-            if not node:
-                return False
-            if node == target:
-                return True
-            return contains(node.left, target) or contains(node.right, target)
-
-        if ((root == p and contains(root, q)) or (root == q and contains(root, p))
-            or (contains(root.left, p) and contains(root.right, q))
-            or (contains(root.right, p) and contains(root.left, q))):
+        if ((root == p or root == q)
+         or (p.val < root.val and q.val > root.val)
+         or (p.val > root.val and q.val < root.val)):
             return root
-
-        if contains(root.left, p) and contains(root.left, q):
+        
+        if p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left, p, q)
-
-        if contains(root.right, p) and contains(root.right, q):
+        
+        if p.val > root.val and q.val > root.val:
             return self.lowestCommonAncestor(root.right, p, q)
