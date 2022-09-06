ee4a1e0337157592e4c847afc98b3397b6b1ecbb
John <zathrath03@gmail.com>
2022-09-05 19:14:03 -0700

    Time: 32 ms (93.64%), Space: 13.9 MB (9.43%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,9 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:        
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+        if n <= 1:
+            return n
 
         sequence = [0, 1]
 
