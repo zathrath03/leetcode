e07207f01b739a08875bbe53c424d9be1458060a
John <zathrath03@gmail.com>
2022-09-05 19:17:39 -0700

    Time: 31 ms (94.67%), Space: 13.9 MB (9.43%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,11 +1,8 @@
 class Solution:
+    cache = {0: 0, 1: 1}
+    
     def fib(self, n: int) -> int:        
-        if n <= 1:
-            return n
-
-        sequence = [0, 1]
-
-        for _ in range(n-1):
-            sequence.append(sum(sequence[-2:]))
-
-        return sequence[n]
+        if n in self.cache:
+            return self.cache[n]
+        self.cache[n] = self.fib(n-1) + self.fib(n-2)
+        return self.cache[n]
