f9dacb5ed988bca9966ac18e735e08edd664358b
John <zathrath03@gmail.com>
2022-09-19 17:14:29 -0700

    Time: 195 ms (26.48%), Space: 24 MB (67.29%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -1,31 +1,10 @@
 class Solution:
-    def parseString(self, string: str):
-        files = []
-        contents = []
-
-        splits = string.split()
-        directory = splits[0]
-        splits = splits[1:]
-
-        for file in splits:
-            file_and_contents = file.removesuffix(')').split('(')
-            files.append(file_and_contents[0])
-            contents.append(file_and_contents[1])
-
-        return directory, files, contents
-
-    def findDuplicate(self, paths: list[str]) -> list[list[str]]:
-        contents_locations = {}
-        output = []
-
-        for string in paths:
-            directory, files, contents = self.parseString(string)
-            for i, content in enumerate(contents):
-                contents_locations[content] = (
-                    contents_locations.get(content, [])
-                    + [f'{directory}/{files[i]}'])
-        
-        for files in contents_locations.values():
-            if len(files) > 1:
-                output.append(files)
-        return output
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        maps = defaultdict(list)
+        for path in paths:
+            strs = path.split()
+            root = strs[0]
+            for s in strs[1:]:
+                file_name, _, content = s.partition('(')
+                maps[content].append(root+'/'+file_name)
+        return [x for x in maps.values() if len(x) > 1]
