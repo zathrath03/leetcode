680ba9454421c6fa954b7e80c5f0dfdb780d6466
John <zathrath03@gmail.com>
2022-10-10 07:12:20 -0700

    Time: 28 ms (97.06%), Space: 13.9 MB (13.13%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,12 +1,12 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
         length = len(palindrome)
+        if length == 1:
+            return ""
         for i in range(length // 2):
             if palindrome[i] != 'a':
                 return palindrome[:i] + 'a' + palindrome[i+1:]
-        if length > 1:
-            if palindrome[-1] != 'a':
-                return palindrome[:-1] + 'a'
-            else:
-                return palindrome[:-1] + 'b'
-        return ""
+        if palindrome[-1] != 'a':
+            return palindrome[:-1] + 'a'
+        else:
+            return palindrome[:-1] + 'b'
