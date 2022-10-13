a35654f29a7bcba58b7bfef47e382edab7ad041b
John <zathrath03@gmail.com>
2022-10-13 13:23:55 -0700

    Time: 90 ms (10.74%), Space: 14.4 MB (15.01%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -1,3 +1,12 @@
+from gc import get_referrers
+
 class Solution:
     def deleteNode(self, node):
-        node.val, node.next = node.next.val, node.next.next
+        referrers = get_referrers(node)
+        if referrers:
+            parent = referrers[0]
+            if parent.get('next'):
+                parent['next'] = node.next
+                del node
+        else:
+            node.val, node.next = node.next.val, node.next.next
