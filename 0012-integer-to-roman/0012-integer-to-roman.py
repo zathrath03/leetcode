3282d694b60cb4a9e883b1ac2c4ff20bf894279b
John <zathrath03@gmail.com>
2022-10-19 20:33:18 -0700

    Time: 111 ms (28.07%), Space: 14 MB (35.57%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,17 +1,16 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        romans = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
+        romans = {1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC",
+                  50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"}
         if num in romans:
             return romans[num]
-        
-        sorted_roman_integers = sorted(romans.keys(), reverse=True)
-        
+
         output = ""
-        for i in range(len(sorted_roman_integers)):
+        for key in romans:
             while num > 0:
-                if sorted_roman_integers[i] > num:
+                if key > num:
                     break
-                num -= sorted_roman_integers[i]
-                output += romans[sorted_roman_integers[i]]
-            
+                num -= key
+                output += romans[key]
+
         return output
