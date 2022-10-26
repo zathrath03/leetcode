9709e5367c284caff108f2d93c98b09bb5ef7a28
John <zathrath03@gmail.com>
2022-10-25 19:34:40 -0700

    Time: 2615 ms (21.52%), Space: 33.1 MB (88.49%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,14 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: list[int], k: int) -> bool:
         # if sum(nums[i:j])%k == 0, then sum(nums[:i])%k == sum(nums[:j])%k
-        remainders = {0: 0}
+        remainders = {0: -1}
 
         running_sum = 0
         for i, num in enumerate(nums):
             running_sum += num
             remainder = running_sum % k
             if remainder not in remainders:
-                remainders[remainder] = i + 1
-            elif remainders[remainder] < i:
+                remainders[remainder] = i
+            elif remainders[remainder] < i - 1:
                 return True
         return False
