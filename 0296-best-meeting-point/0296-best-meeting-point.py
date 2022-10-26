e2e03900aa8936e899e5211a8693c0463ed2acdc
John <zathrath03@gmail.com>
2022-10-25 19:44:38 -0700

    Time: 333 ms (23.94%), Space: 14.7 MB (99.37%) - LeetHub

---
+++ 0296-best-meeting-point/0296-best-meeting-point.py
@@ -0,0 +1,23 @@
+# Not my code
+# https://leetcode.com/problems/best-meeting-point/discuss/74193/Java-2msPython-40ms-two-pointers-solution-no-median-no-sort-with-explanation
+
+class Solution:
+    def minTotalDistance(self, grid):
+        row_sum = list(map(sum, grid))
+        col_sum = list(map(sum, zip(*grid)))
+
+        def minTotalDistance1D(vec):
+            i, j = -1, len(vec)
+            d = left = right = 0
+            while i != j:
+                if left < right:
+                    d += left
+                    i += 1
+                    left += vec[i]
+                else:
+                    d += right
+                    j -= 1
+                    right += vec[j]
+            return d
+
+        return minTotalDistance1D(row_sum) + minTotalDistance1D(col_sum)
