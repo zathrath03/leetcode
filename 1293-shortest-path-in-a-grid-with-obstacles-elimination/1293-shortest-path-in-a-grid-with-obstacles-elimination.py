6719e306663b109e3b9b30926e76309f857199df
John <zathrath03@gmail.com>
2022-10-30 15:36:28 -0700

    Time: 231 ms (59.11%), Space: 15.4 MB (67.27%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,36 +1,34 @@
 class Solution:
-    def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        rows, cols = len(grid), len(grid[0])
-        target = (rows - 1, cols - 1)
+    def shortestPath(self, grid: list[list[int]], k: int) -> int:
+        num_rows = len(grid)
+        num_cols = len(grid[0])
 
-        # if we have sufficient quotas to eliminate the obstacles in the worst case,
-        # then the shortest distance is the Manhattan distance
-        if k >= rows + cols - 2:
-            return rows + cols - 2
+        if k > num_rows + num_cols - 3:
+            return num_rows + num_cols - 2
 
-        # (row, col, remaining quota to eliminate obstacles)
+        target = (num_rows - 1, num_cols - 1)
         state = (0, 0, k)
-        # (steps, state)
-        queue = deque([(0, state)])
-        seen = set([state])
+
+        visited: set[tuple[int, int, int]] = set()
+        visited.add(state)
+
+        queue: deque[tuple[int, tuple[int, int, int]]] = deque()
+        queue.append((0, state))
 
         while queue:
-            steps, (row, col, k) = queue.popleft()
+            steps, (row, col, remaining) = queue.popleft()
 
-            # we reach the target here
             if (row, col) == target:
                 return steps
 
-            # explore the four directions in the next step
-            for new_row, new_col in [(row, col + 1), (row + 1, col), (row, col - 1), (row - 1, col)]:
-                # if (new_row, new_col) is within the grid boundaries
-                if (0 <= new_row < rows) and (0 <= new_col < cols):
-                    new_eliminations = k - grid[new_row][new_col]
-                    new_state = (new_row, new_col, new_eliminations)
-                    # add the next move in the queue if it qualifies
-                    if new_eliminations >= 0 and new_state not in seen:
-                        seen.add(new_state)
-                        queue.append((steps + 1, new_state))
+            for row, col in [(row + 1, col), (row, col + 1),
+                             (row - 1, col), (row, col - 1)]:
+                if (0 <= row < num_rows) and (0 <= col < num_cols):
+                    new_remaining = remaining - grid[row][col]
+                    state = (row, col, new_remaining)
 
-        # did not reach the target
+                    if remaining >= 0 and state not in visited:
+                        queue.append((steps + 1, state))
+                        visited.add(state)
+
         return -1
