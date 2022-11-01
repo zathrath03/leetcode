6c574e05da75c2291e694047e491feb6681bde5c
John <zathrath03@gmail.com>
2022-11-01 13:33:33 -0700

    Time: 477 ms (45.60%), Space: 14.3 MB (84.78%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -7,17 +7,21 @@ class Solution:
             output.append(self.find_output(col))
         return output
 
-    def find_output(self, col):
+    def find_output(self, col: int) -> int:
         grid = self.grid
         num_rows = len(grid)
         num_cols = len(grid[0])
+        output = -1
         for row in range(num_rows):
             slant = grid[row][col]
             boundary_col = col + slant
             if (boundary_col < 0 or boundary_col >= num_cols
                     or grid[row][boundary_col] == -slant):
-                return -1
+                output = -1
+                break
             elif row == num_rows - 1:
-                return col + slant
+                output = col + slant
+                break
             else:
                 col += slant
+        return output
