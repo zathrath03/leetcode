48ea295b8dd0befe17851d4f1dd4df7928540592
John <zathrath03@gmail.com>
2022-11-03 15:50:43 -0700

    Time: 4539 ms (5.02%), Space: 38.1 MB (94.77%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -4,42 +4,42 @@ class Solution:
 
         for word in words:
             self.determine_if_needed(word)
-        self.increment_output_if_single_double_letter_word()
+        self.increment_output_if_double_letter_word_remains()
         return self.output
 
-    def increment_output_if_single_double_letter_word(self):
-        if self.double_letter_words:
-            self.output += 2
+    def init(self) -> None:
+        self.needed_words: defaultdict[str, int] = defaultdict(int)
+        self.output = 0
+        self.double_letter_words = 0
 
-    def determine_if_needed(self, word):
+    def determine_if_needed(self, word: str) -> None:
         if word in self.needed_words:
             self.handle_needed_words(word)
         else:
             self.handle_new_words(word)
 
-    def init(self):
-        self.needed_words: defaultdict[str, int] = defaultdict(int)
-        self.output = 0
-        self.double_letter_words = 0
+    def increment_output_if_double_letter_word_remains(self) -> None:
+        if self.double_letter_words:
+            self.output += 2
 
-    def handle_new_words(self, word):
+    def handle_needed_words(self, word: str) -> None:
+        self.decrement_double_letters(word)
+        self.decrement_needed_words(word)
+        self.output += 4
+
+    def handle_new_words(self, word: str) -> None:
         self.increment_double_letters(word)
         self.needed_words[word[::-1]] += 1
 
-    def increment_double_letters(self, word):
+    def decrement_double_letters(self, word: str) -> None:
         if word[0] == word[1]:
-            self.double_letter_words += 1
+            self.double_letter_words -= 1
 
-    def handle_needed_words(self, word):
-        self.decrement_double_letters(word)
-        self.decrement_candidates(word)
-        self.output += 4
-
-    def decrement_candidates(self, word):
+    def decrement_needed_words(self, word: str) -> None:
         self.needed_words[word] -= 1
         if self.needed_words[word] == 0:
             del self.needed_words[word]
 
-    def decrement_double_letters(self, word):
+    def increment_double_letters(self, word: str) -> None:
         if word[0] == word[1]:
-            self.double_letter_words -= 1
+            self.double_letter_words += 1
