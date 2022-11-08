735b0f35c8eb5361588548e8b76582e5b2efcf20
John <zathrath03@gmail.com>
2022-11-08 14:43:53 -0800

    Time: 91 ms (8.17%), Space: 13.9 MB (61.84%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -3,8 +3,7 @@ class Solution:
         
         while True:
             for i in range(len(s) - 1):
-                char = s[i]
-                char_val = ord(char)
+                char_val = ord(s[i])
                 next_char_val = ord(s[i+1])
                 if abs(char_val - next_char_val) == 32:
                     s = s[:i] + s[i+2:]
