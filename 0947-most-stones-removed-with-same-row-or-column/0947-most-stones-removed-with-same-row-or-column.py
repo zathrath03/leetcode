8e5abc08cfb87a9a509a380dcca7ac9a6e48469f
John <zathrath03@gmail.com>
2022-11-13 20:22:45 -0800

    Time: 401 ms (51.17%), Space: 14.6 MB (72.25%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,13 @@
+# I wish I could take credit for this solution,
+# but I'm not even sure I understand it...
+
+class Solution:
+    def removeStones(self, points):
+        uf = {}
+        def find(x):
+            if x != uf.setdefault(x, x):
+                uf[x] = find(uf[x])
+            return uf[x]
+        for i, j in points:
+            uf[find(i)] = find(~j)
+        return len(points) - len({find(x) for x in uf})
