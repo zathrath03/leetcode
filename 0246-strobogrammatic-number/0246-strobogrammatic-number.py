bd2d64cf71f0a076d1f5faf20c4f37cd34fecb34
John <zathrath03@gmail.com>
2022-11-22 13:26:25 -0800

    Time: 41 ms (77.65%), Space: 14 MB (14.75%) - LeetHub

---
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        strobogrammatic_numbers = {"0": "0", "1": "1", "6": "9", "8": "8", "9": "6"}
+        
+        lft = 0
+        rgt = len(num) - 1
+        
+        while lft <= rgt:
+            left = num[lft]
+            right = num[rgt]
+            
+            if (left not in strobogrammatic_numbers
+                 or right not in strobogrammatic_numbers
+                 or strobogrammatic_numbers[left] != right):
+                return False
+            lft += 1
+            rgt -= 1
+                
+        return True
