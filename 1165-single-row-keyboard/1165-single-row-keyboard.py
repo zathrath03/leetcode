bfcc2ffe4879f6dd88b1bada9b7c3feaca178c4b
John <zathrath03@gmail.com>
2022-11-30 17:02:41 -0800

    Time: 64 ms (79.75%), Space: 14 MB (50.00%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.py
+++ 1165-single-row-keyboard/1165-single-row-keyboard.py
@@ -1,12 +1,11 @@
 class Solution:
     def calculateTime(self, keyboard: str, word: str) -> int:
-        key_map = {}
+        key_map = {key: index for index, key in enumerate(keyboard)}
         time = last_time = 0
         
         for char in word:
-            char_index = key_map.setdefault(char, keyboard.index(char))
-            time += abs(char_index - last_time)
-            last_time = char_index
+            time += abs(key_map[char] - last_time)
+            last_time = key_map[char]
             
         return time
             
