a3b77dea5d4cf81149c1827004e5eb66ef1b3a61
John <zathrath03@gmail.com>
2022-12-30 18:32:09 -0800

    Time: 88 ms (98.04%), Space: 41.6 MB (17.65%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cs
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cs
@@ -1,80 +1,53 @@
-/**
- * // This is BinaryMatrix's API interface.
- * // You should not implement it, or speculate about its implementation
- * class BinaryMatrix {
- *     public int Get(int row, int col) {}
- *     public IList<int> Dimensions() {}
- * }
- */
-
-
 class Solution
-{
-    private static int MAX_MATRIX_LENGTH = 100;
-    private BinaryMatrix binaryMatrix;
-    private int rows;
-    private int cols;
-    private int leftmostColumnWithOne = MAX_MATRIX_LENGTH + 1;
+    {
+    private const int MaxMatrixLength = 100;
+    private BinaryMatrix _binaryMatrix;
+    private int _rows;
+    private int _cols;
+    private int _leftmostColumnWithOne = MaxMatrixLength + 1;
 
     public int LeftMostColumnWithOne(BinaryMatrix binaryMatrixParam)
     {
-        binaryMatrix = binaryMatrixParam;
-        rows = binaryMatrix.Dimensions()[0];
-        cols = binaryMatrix.Dimensions()[1];
+        _binaryMatrix = binaryMatrixParam;
+        _rows = _binaryMatrix.Dimensions()[0];
+        _cols = _binaryMatrix.Dimensions()[1];
 
         var rowsWithOnes = GetRowsWithOnes();
 
-        foreach (var row in rowsWithOnes)
-        {
-            BinarySearchRow(row);
-        }
+        foreach (var row in rowsWithOnes) BinarySearchRow(row);
 
-        if (leftmostColumnWithOne == MAX_MATRIX_LENGTH + 1)
-        {
+        if (_leftmostColumnWithOne == MaxMatrixLength + 1)
             return -1;
-        }
-        else
-        {
-            return leftmostColumnWithOne;
-        }
+        return _leftmostColumnWithOne;
     }
 
-    public HashSet<int> GetRowsWithOnes()
+    private HashSet<int> GetRowsWithOnes()
     {
         var rowsWithOnes = new HashSet<int>();
-        for (var row = 0; row < rows; row++)
-        {
-            if (binaryMatrix.Get(row, cols - 1) == 1)
-            {
+        for (var row = 0; row < _rows; row++)
+            if (_binaryMatrix.Get(row, _cols - 1) == 1)
                 rowsWithOnes.Add(row);
-            }
-        }
         return rowsWithOnes;
     }
 
-    public void BinarySearchRow(int row)
+    private void BinarySearchRow(int row)
     {
         var left = 0;
-        var right = cols - 1;
+        var right = _cols - 1;
 
         while (left <= right)
         {
             var mid = (left + right) / 2;
-            if (binaryMatrix.Get(row, mid) == 1)
+            if (_binaryMatrix.Get(row, mid) == 1)
             {
-                leftmostColumnWithOne = Math.Min(leftmostColumnWithOne, mid);
+                _leftmostColumnWithOne = Math.Min(_leftmostColumnWithOne, mid);
                 right = mid - 1;
             }
             else
             {
                 left = mid + 1;
-                if (left >= leftmostColumnWithOne)
-                {
+                if (left >= _leftmostColumnWithOne)
                     break;
-                }
-            }
-            {
-
             }
         }
     }
