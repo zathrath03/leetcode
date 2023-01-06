7d79364f30a4649ca41c23a10d947b73a9fb87f5
John <zathrath03@gmail.com>
2023-01-05 16:30:31 -0800

    Time: 28 ms (88.45%), Space: 13.9 MB (55.14%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -3,11 +3,10 @@ class Solution:
         confusing_digits = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
         n = str(n)
         rotated_n = []
-        
+
         for digit in n[::-1]:
             if digit not in confusing_digits:
                 return False
             rotated_n.append(confusing_digits[digit])
-            
+
         return "".join(rotated_n) != n
-            
