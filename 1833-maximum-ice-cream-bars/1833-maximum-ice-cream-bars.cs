ead525df1084419ec4c4944d3079182d964e30aa
John <zathrath03@gmail.com>
2023-01-05 16:16:32 -0800

    Time: 561 ms (16.67%), Space: 47.7 MB (94.44%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxIceCream(int[] costs, int coins) {
+        Array.Sort(costs);
+        var count = 0;
+        
+        foreach (var cost in costs){
+            coins -= cost;
+            if (coins < 0) break;
+            count += 1;
+        }
+        
+        return count;
+    }
+}
