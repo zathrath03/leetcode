f9ab6c0b1bed988d96bbc42c99eaa43aae2f06fa
John <zathrath03@gmail.com>
2023-01-07 20:25:43 -0800

    Time: 3516 ms (5.55%), Space: 44.1 MB (5.02%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,19 +1,27 @@
 class Solution:
     def maxPoints(self, points: list[list[int]]) -> int:
-        if len(points) < 3: return len(points)
+        if len(points) < 3:
+            return len(points)
         lines = self.getLines(points)
         self.distributePointsToLines(points, lines)
         return self.findMaxPoints(lines)
 
-    def getLines(self, points: list[int]) -> dict[tuple[float, float], set[tuple[int, int]]]:
+    def getLines(
+        self, points: list[list[int]]
+    ) -> dict[tuple[float, float], set[tuple[int, int]]]:
         lines = defaultdict(set)
         for pt1 in points:
             for pt2 in points:
-                if pt1 is pt2: continue
+                if pt1 is pt2:
+                    continue
                 self.addEquationsToLines(pt1, pt2, lines)
         return lines
 
-    def distributePointsToLines(self, points: list[list[int]], lines: dict[tuple[float, float], set[tuple[int, int]]]) -> None:
+    def distributePointsToLines(
+        self,
+        points: list[list[int]],
+        lines: dict[tuple[float, float], set[tuple[int, int]]],
+    ) -> None:
         for pt in points:
             for m, b in lines.keys():
                 if m == float(inf) and b == pt[0]:
@@ -21,19 +29,27 @@ class Solution:
                 if pt[1] == m * pt[0] + b:
                     lines[(m, b)].add(tuple(pt))
 
-    def findMaxPoints(self, lines: dict[tuple[float, float], set[tuple[int, int]]]) -> int:
+    def findMaxPoints(
+        self, lines: dict[tuple[float, float], set[tuple[int, int]]]
+    ) -> int:
         most_points = 0
         for pts in lines.values():
             most_points = max(most_points, len(pts))
         return most_points
 
-    def addEquationsToLines(self, pt1: list[int,int], pt2: list[int,int], lines: dict[tuple[float, float], set[tuple[int, int]]]) -> None:
+    def addEquationsToLines(
+        self,
+        pt1: list[int],
+        pt2: list[int],
+        lines: dict[tuple[float, float], set[tuple[int, int]]],
+    ) -> None:
         equation = self.getEquation(pt1, pt2)
         lines[equation].add(tuple(pt1))
         lines[equation].add(tuple(pt2))
 
-    def getEquation(self, p1: list[int, int], p2: list[int, int]) -> tuple[float, float]:
-        if p2[0] == p1[0]: return float(inf), p2[0]
+    def getEquation(self, p1: list[int], p2: list[int]) -> tuple[float, float]:
+        if p2[0] == p1[0]:
+            return float(inf), p2[0]  # vertical line
         m = (p2[1] - p1[1]) / (p2[0] - p1[0])
         b = p1[1] - m * p1[0]
         return m, b
