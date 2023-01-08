83e252359c3c58efc9cfc8de521da79f515dd66c
John <zathrath03@gmail.com>
2023-01-07 21:13:45 -0800

    Time: 7876 ms (5.02%), Space: 44.1 MB (5.02%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,40 +1,41 @@
 class Solution:
-
     def maxPoints(self, points: list[list[int]]) -> int:
         if len(points) < 3:
             return len(points)
-        lines = self.getLines(points)
-        self.distributePointsToLines(points, lines)
-        return self.findMaxPoints(lines)
+        line_point_map = self.getLines(points)
+        self.distributePointsToLines(points, line_point_map)
+        return self.findMaxPoints(line_point_map)
 
     def getLines(
         self, points: list[list[int]]
     ) -> dict[tuple[float, float], set[tuple[int, int]]]:
-        lines = defaultdict(set)
+        line_point_map: dict[
+            tuple[float, float], set[tuple[int, int]]
+        ] = defaultdict(set)
         for pt1 in points:
             for pt2 in points:
                 if pt1 == pt2:
                     continue
-                self.addEquationsToLines(pt1, pt2, lines)
-        return lines
+                self.addEquationsToMap(pt1, pt2, line_point_map)
+        return line_point_map
 
     def distributePointsToLines(
         self,
         points: list[list[int]],
-        lines: dict[tuple[float, float], set[tuple[int, int]]],
+        line_point_map: dict[tuple[float, float], set[tuple[int, int]]],
     ) -> None:
         for point in points:
-            for line in lines.keys():
-                self.addPointToLine(point, line, lines)
+            for line in line_point_map.keys():
+                self.addPointToLine(point, line, line_point_map)
 
     def addPointToLine(
         self,
         point: list[int],
         line: tuple[float, float],
-        lines: dict[tuple[float, float], set[tuple[int, int]]],
+        line_point_map: dict[tuple[float, float], set[tuple[int, int]]],
     ) -> None:
         if self.isOnVerticalLine(point[0], line) or self.isOnLine(point, line):
-            lines[line].add(tuple(point))
+            line_point_map[line].add(tuple(point))
 
     def isOnLine(self, point: list[int], line: tuple[float, float]) -> bool:
         m, b = line
@@ -46,26 +47,37 @@ class Solution:
         return m == float(inf) and b == x
 
     def findMaxPoints(
-        self, lines: dict[tuple[float, float], set[tuple[int, int]]]
+        self, line_point_map: dict[tuple[float, float], set[tuple[int, int]]]
     ) -> int:
         most_points = 0
-        for points in lines.values():
-            most_points = max(most_points, len(points))
+        for pts in line_point_map.values():
+            most_points = max(most_points, len(pts))
         return most_points
 
-    def addEquationsToLines(
+    def addEquationsToMap(
         self,
         pt1: list[int],
         pt2: list[int],
-        lines: dict[tuple[float, float], set[tuple[int, int]]],
+        line_point_map: dict[tuple[float, float], set[tuple[int, int]]],
     ) -> None:
         equation = self.getEquation(pt1, pt2)
-        lines[equation].add(tuple(pt1))
-        lines[equation].add(tuple(pt2))
+        line_point_map[equation].add(tuple(pt1))
+        line_point_map[equation].add(tuple(pt2))
 
     def getEquation(self, p1: list[int], p2: list[int]) -> tuple[float, float]:
-        if p2[0] == p1[0]:
-            return float(inf), p2[0]  # vertical line
-        m = (p2[1] - p1[1]) / (p2[0] - p1[0])
-        b = p1[1] - m * p1[0]
+        if self.isVerticalLine(p1, p2):
+            return float(inf), p1[0]
+        m = self.getSlope(p1, p2)
+        b = self.getYIntercept(p1, m)
         return m, b
+
+    def isVerticalLine(self, p1: list[int], p2: list[int]) -> bool:
+        return p1[0] == p2[0]
+
+    def getYIntercept(self, p1: list[int], m: float) -> float:
+        return p1[1] - m * p1[0]
+
+    def getSlope(self, p1: list[int], p2: list[int]) -> float:
+        x1, y1 = p1
+        x2, y2 = p2
+        return (y2 - y1) / (x2 - x1)
