2ba1ebf46db9038953bd19affe897a0b678c99c5
John <zathrath03@gmail.com>
2023-01-10 17:31:12 -0800

    Time: 279 ms (90.00%), Space: 73.8 MB (20.00%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public Dictionary<int, HashSet<int>> AdjacencyList = new Dictionary<int, HashSet<int>>();
+    public IList<bool> HasApple;
+    
+    public int MinTime(int n, int[][] edges, IList<bool> hasApple) {
+        this.HasApple = hasApple;
+        
+        foreach (var edge in edges){
+            if (! AdjacencyList.TryGetValue(edge[0], out var list)){
+                list = new HashSet<int>();
+                AdjacencyList[edge[0]] = list;
+            }
+            list.Add(edge[1]);
+            
+            if (! AdjacencyList.TryGetValue(edge[1], out list)) {
+                list = new HashSet<int>();
+                AdjacencyList[edge[1]] = list;
+            }
+            list.Add(edge[0]);
+        }
+        
+        return dfs(0, -1);
+    }
+    
+    public int dfs(int node, int parent){
+        if (! AdjacencyList.ContainsKey(node)) return 0;
+        
+        var totalTime = 0;
+        foreach (var child in AdjacencyList[node]) {
+            if (child == parent) continue;
+            var child_time = dfs(child, node);
+            if (child_time > 0 || HasApple[child])
+                totalTime += child_time + 2;
+        }
+        return totalTime;
+    } 
+}
