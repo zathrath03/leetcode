150bae89bb3db5bf3a7dc3d9570b233c981dfad9
John <zathrath03@gmail.com>
2023-01-10 19:43:09 -0800

    Time: 188 ms (83.65%), Space: 50.6 MB (51.92%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cs
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cs
@@ -1,6 +1,15 @@
 public class Solution {
     public long MinimumHealth(int[] damage, int armor) {
-        var maxDamage = damage.Aggregate(0L, (sum, dmg) => sum + dmg);
-        return 1 + maxDamage - Math.Min(damage.Max(), armor);
+        long totalDamage = 0;
+        var maxDamageSeen = 0;
+        
+        foreach (var d in damage){
+            maxDamageSeen = Math.Max(d, maxDamageSeen);
+            totalDamage += d;
+        }
+        
+        var blockedDamage = Math.Min(armor, maxDamageSeen);
+        
+        return 1 + totalDamage - blockedDamage;
     }
 }
