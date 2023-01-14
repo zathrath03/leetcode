a55a384c69ff3be9e62a3117500a8411c89ef30f
John <zathrath03@gmail.com>
2023-01-13 16:47:34 -0800

    Time: 41 ms (87.93%), Space: 13.8 MB (94.83%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,34 +1,33 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
         char_map = defaultdict(set)
-        output = []
-        
+
         for c1, c2 in zip(s1, s2):
             char_map[c1].add(c2)
             char_map[c2].add(c1)
-                
+
         memo = {}        
         def bfs(char: str) -> str:
             if char in memo:
                 return memo[char]
-            
+
             if char not in char_map:
                 return char
-            
+
             smallest_char = char
             seen = set()
             queue = {char}
-            
+
             while queue:
                 char = queue.pop()
                 if char in seen: continue
                 seen.add(char)
                 smallest_char = min(char, smallest_char)
                 queue |= char_map[char]
-                
+
             for char in seen:
                 memo[char] = smallest_char
-                
+
             return smallest_char
-            
+
         return "".join(bfs(char) for char in baseStr)
