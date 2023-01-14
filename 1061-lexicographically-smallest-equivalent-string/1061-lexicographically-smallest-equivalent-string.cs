8d872d082cf7f4c47d03bfbabde3c03b31393399
John <zathrath03@gmail.com>
2023-01-13 19:09:12 -0800

    Time: 100 ms (50.00%), Space: 39.4 MB (50.00%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cs
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cs
@@ -1,80 +1,82 @@
-    public class Solution
+public class Solution
+{
+    private static Dictionary<char, HashSet<char>> _charMap;
+    private static Dictionary<char, char> _memo;
+    public string SmallestEquivalentString(string s1, string s2, string baseStr)
     {
-        public string SmallestEquivalentString(string s1, string s2, string baseStr)
+        _charMap = new Dictionary<char, HashSet<char>>();
+        _memo = new Dictionary<char, char>();
+
+        foreach (var (c1, c2) in s1.Zip(s2, (first, second) => (first, second)))
         {
-            var charMap = new Dictionary<char, HashSet<char>>();
-            var memo = new Dictionary<char, char>();
-
-            foreach (var (c1, c2) in Enumerable.Zip(s1, s2))
-            {
-                AddToMap(charMap, c1, c2);
-                AddToMap(charMap, c2, c1);
-            }
-
-            return string.Join("", baseStr.Select(c => BFS(memo, charMap, c)));
+            AddToMap(c1, c2);
+            AddToMap(c2, c1);
         }
 
-        private static void AddToMap(IDictionary<char, HashSet<char>> charMap, char key, char val)
+        return string.Join("", baseStr.Select(BFS));
+    }
+
+    private static void AddToMap(char key, char val)
+    {
+        if (_charMap.ContainsKey(key))
         {
-            if (charMap.ContainsKey(key))
-            {
-                charMap[key].Add(val);
-            }
-            else
-            {
-                charMap[key] = new HashSet<char>() { val };
-            }
+            _charMap[key].Add(val);
         }
-
-        private char BFS(IDictionary<char, char> memo, IReadOnlyDictionary<char, HashSet<char>> charMap, char c)
+        else
         {
-            if (memo.ContainsKey(c)) return memo[c];
-            if (!charMap.ContainsKey(c)) return c;
-
-            var smallestChar = FindSmallestChar(charMap, c, out var seen);
-
-            UpdateMemo(memo, seen, smallestChar);
-
-            return smallestChar;
-        }
-
-        private static void UpdateMemo(IDictionary<char, char> memo, HashSet<char> seen, char smallestChar)
-        {
-            foreach (var cha in seen)
-            {
-                memo[cha] = smallestChar;
-            }
-        }
-
-        private static char FindSmallestChar(IReadOnlyDictionary<char, HashSet<char>> charMap, char c, out HashSet<char> seen)
-        {
-            var smallestChar = c;
-            seen = new HashSet<char>();
-            var queue = new HashSet<char>() { c };
-
-            smallestChar = SmallestCharLoop(charMap, seen, queue, smallestChar);
-
-            return smallestChar;
-        }
-
-        private static char SmallestCharLoop(IReadOnlyDictionary<char, HashSet<char>> charMap, ISet<char> seen, HashSet<char> queue, char smallestChar)
-        {
-            while (queue.Count > 0)
-            {
-                var ch = Pop(queue);
-                if (seen.Contains(ch)) continue;
-                seen.Add(ch);
-                smallestChar = smallestChar.CompareTo(ch) < 0 ? smallestChar : ch;
-                queue.UnionWith(charMap[ch]);
-            }
-
-            return smallestChar;
-        }
-
-        private static char Pop(ICollection<char> queue)
-        {
-            var c = queue.First();
-            queue.Remove(c);
-            return c;
+            _charMap[key] = new HashSet<char>() { val };
         }
     }
+
+    private char BFS(char c)
+    {
+        if (_memo.ContainsKey(c)) return _memo[c];
+        if (!_charMap.ContainsKey(c)) return c;
+
+        var smallestChar = FindSmallestChar(c, out var seen);
+
+        UpdateMemo(seen, smallestChar);
+
+        return smallestChar;
+    }
+
+    private static char FindSmallestChar(char c, out HashSet<char> seen)
+    {
+        var smallestChar = c;
+        seen = new HashSet<char>();
+        var queue = new HashSet<char>() { c };
+
+        smallestChar = SmallestCharLoop(seen, queue, smallestChar);
+
+        return smallestChar;
+    }
+
+    private static void UpdateMemo(HashSet<char> seen, char smallestChar)
+    {
+        foreach (var c in seen)
+        {
+            _memo[c] = smallestChar;
+        }
+    }
+
+    private static char SmallestCharLoop(ISet<char> seen, ISet<char> queue, char smallestChar)
+    {
+        while (queue.Count > 0)
+        {
+            var c = Pop(queue);
+            if (seen.Contains(c)) continue;
+            seen.Add(c);
+            smallestChar = smallestChar.CompareTo(c) < 0 ? smallestChar : c;
+            queue.UnionWith(_charMap[c]);
+        }
+
+        return smallestChar;
+    }
+
+    private static char Pop(ICollection<char> queue)
+    {
+        var c = queue.First();
+        queue.Remove(c);
+        return c;
+    }
+}
