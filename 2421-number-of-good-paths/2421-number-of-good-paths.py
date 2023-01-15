e756c18cb7427a76a64a4456df4a77c53673379d
John <zathrath03@gmail.com>
2023-01-14 19:11:00 -0800

    Time: 1685 ms (99.80%), Space: 31.5 MB (99.60%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.py
+++ 2421-number-of-good-paths/2421-number-of-good-paths.py
@@ -1,29 +1,29 @@
 class Solution:
     def numberOfGoodPaths(self, vals, edges):
         edges.sort(key=lambda x: max(vals[x[0]], vals[x[1]]))
-        
+
         n = len(vals)
-        par = [i for i in range(n)]; 
+        par = [i for i in range(n)]
         rank = [1]*n
-        
-        def find(p): #find parent of node i
+
+        def find(p):
             while par[p] != p:
                 par[p]= par[par[p]]
                 p = par[p]
             return p
-        
+
         goodPaths = n
-        
+
         for a,b in edges:
             parent_a, parent_b = find(a), find(b)
-            
+
             if vals[parent_a] == vals[parent_b]:
                 goodPaths += rank[parent_a] * rank[parent_b]
                 par[parent_a] = parent_b
                 rank[parent_b] += rank[parent_a]
-            elif vals[parent_a] > vals[parent_b]: 
+            elif vals[parent_a] > vals[parent_b]:
                 par[parent_b] = parent_a
-            else: 
+            else:
                 par[parent_a] = parent_b
-                
+
         return goodPaths
