32db5da3dc1d58e8f2bb667ce8664e26b14d962f
John <zathrath03@gmail.com>
2023-01-16 16:37:16 -0800

    Time: 91 ms (98.72%), Space: 15 MB (53.53%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -4,7 +4,7 @@ class Solution:
         for c in s:
             if c == '1':
                 number_of_ones_encountered += 1
-            else:
-                flip_count = min(number_of_ones_encountered, flip_count + 1)
-                
+            elif number_of_ones_encountered > flip_count:
+                flip_count += 1
+
         return flip_count
