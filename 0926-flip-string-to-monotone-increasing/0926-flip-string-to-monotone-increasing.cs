8c39ffdb8c21b30445619362632357363f67f6d0
John <zathrath03@gmail.com>
2023-01-16 16:49:09 -0800

    Time: 88 ms (59.38%), Space: 43.5 MB (65.63%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cs
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int MinFlipsMonoIncr(string s) {
         int flipCount = 0;
         int onesCount = 0;
-        
+
         foreach (var c in s) {
             if (c == '1')
                 onesCount++;
