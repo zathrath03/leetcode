5413a7604b686c1105338e6ee9c079425b479af7
John <zathrath03@gmail.com>
2023-01-17 18:45:41 -0800

    Time: 153 ms (81.25%), Space: 52.2 MB (32.81%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MaxSubarraySumCircular(int[] nums) {
+        int total, curMax, curMin, maxSum, minSum;
+        total = curMax = curMin = 0;
+        maxSum = minSum = nums[0];
+        
+        foreach (var num in nums){
+            curMax = Math.Max(num, num + curMax);
+            maxSum = Math.Max(curMax, maxSum);
+            curMin = Math.Min(num, num + curMin);
+            minSum = Math.Min(curMin, minSum);
+            total += num;
+        }
+        
+        return maxSum > 0 ? Math.Max(maxSum, total - minSum) : maxSum;
+    }
+}
