6a12c41de971758c9381c8d9d53cd1313e3631f5
John <zathrath03@gmail.com>
2023-01-17 18:45:56 -0800

    Time: 155 ms (78.13%), Space: 52.2 MB (26.56%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cs
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cs
@@ -3,7 +3,6 @@ public class Solution {
         int total, curMax, curMin, maxSum, minSum;
         total = curMax = curMin = 0;
         maxSum = minSum = nums[0];
-        
         foreach (var num in nums){
             curMax = Math.Max(num, num + curMax);
             maxSum = Math.Max(curMax, maxSum);
@@ -11,7 +10,6 @@ public class Solution {
             minSum = Math.Min(curMin, minSum);
             total += num;
         }
-        
         return maxSum > 0 ? Math.Max(maxSum, total - minSum) : maxSum;
     }
 }
