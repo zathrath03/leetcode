d070989e9cce7101fe1c25f15569f424501322f5
John <zathrath03@gmail.com>
2023-01-24 20:22:34 -0800

    Time: 1518 ms (54.02%), Space: 28.2 MB (93.68%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -2,25 +2,25 @@ class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
         if node1 == node2: return node1
         MAX_NODES = 10 ** 5 + 1
-        n1 = {node1}
-        n2 = {node2}
-        ans = MAX_NODES
+        visited1 = {node1}
+        visited2 = {node2}
+        closest_node = MAX_NODES
 
         while True:
             if node1 != -1:
                 node1 = edges[node1]
             if node2 != -1:
                 node2 = edges[node2]
-            if node1 in n1 and node2 in n2: break
+            if node1 in visited1 and node2 in visited2: break
 
-            n1.add(node1)
-            n2.add(node2)
+            visited1.add(node1)
+            visited2.add(node2)
 
-            if node1 in n2:
-                ans = node1
-            if node2 in n1 and node2 < ans:
-                ans = node2
-            if ans < MAX_NODES:
-                return ans
+            if node1 in visited2:
+                closest_node = node1
+            if node2 in visited1 and node2 < closest_node:
+                closest_node = node2
+            if closest_node < MAX_NODES:
+                return closest_node
 
         return -1
