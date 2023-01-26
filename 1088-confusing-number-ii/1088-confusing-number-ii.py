5afb77461bd21f1939283cfdfdaf390a49227ef4
John <zathrath03@gmail.com>
2023-01-25 16:46:30 -0800

    Time: 34 ms (99.11%), Space: 14 MB (20.36%) - LeetHub

--- 1088-confusing-number-ii/1088-confusing-number-ii.py
+++ 1088-confusing-number-ii/1088-confusing-number-ii.py
@@ -1,31 +1,49 @@
-# This solution comes from warmr0bot at
-# https://leetcode.com/problems/confusing-number-ii/discuss/661164/Clean-python3-solution-no-strings
-
 class Solution:
     def confusingNumberII(self, N: int) -> int:
-        rotatables = [1, 0, 6, 8, 9]
-        rotation_map = {1:1, 0:0, 6:9, 8:8, 9:6}        
-        confusing_number_count = 0
-        
-        def count_rotatables(cur_num=0):
-            nonlocal confusing_number_count
-            if cur_num * 10 > N:
-                return
-            for num in rotatables:
-                next_num = cur_num*10 + num
-                if next_num == 0:
-                    continue
-                confusing_number_count += next_num <= N and is_valid_rotation(next_num)
-                count_rotatables(next_num)
-        
-        def is_valid_rotation(num):
-            rotated = 0
-            cur_num = num
-            while cur_num:
-                rotated = rotated * 10 + rotation_map[cur_num%10]
-                cur_num //= 10
-            return rotated != num
-        
-        count_rotatables()
-        
-        return confusing_number_count
+        confusingDigits = [0, 1, 6, 8, 9]
+        mapping = {"0": "0", "1": "1", "6": "9", "8": "8", "9": "6"}
+        S = str(N)
+        Ls = len(S)
+        halfLen = Ls // 2
+        oddLen = bool(Ls % 2)
+
+        ans = 2
+        for l in range(2, Ls):
+            ans += 4 * (5 ** (l - 1) - 5 ** (l // 2 - 1) * (3 if l % 2 == 1 else 1))
+
+        def calc(idx):
+            thisNum = int(S[idx])
+            if thisNum > 8:
+                searchIdx = 4
+            elif thisNum > 6:
+                searchIdx = 3
+            elif thisNum > 1:
+                searchIdx = 2
+            elif thisNum == 1:
+                searchIdx = 1
+            else:
+                searchIdx = 0
+            if idx == Ls - 1:
+                return searchIdx + (confusingDigits[searchIdx] == int(S[idx]))
+            smallerNum = searchIdx - 1 if idx == 0 else searchIdx
+            res = smallerNum * 5 ** (Ls - idx - 1)
+            if idx < halfLen:
+                res -= smallerNum * 5 ** (halfLen - idx - 1) * (3 if oddLen else 1)
+            if idx == halfLen and oddLen:
+                res -= 3 if thisNum > 8 else (2 if thisNum > 1 else (1 if thisNum > 0 else 0))
+            if confusingDigits[searchIdx] == int(S[idx]):
+                res += calc(idx + 1)
+            return res
+
+        ans += calc(0)
+        firstHalfRevNum = 0
+        for i in range(halfLen - 1, -1, -1):
+            if S[i] not in mapping:
+                return ans
+            firstHalfRevNum = firstHalfRevNum * 10 + int(mapping[S[i]])
+        if oddLen:
+            if S[halfLen] in {"0", "1", "8"} and int(S[halfLen + 1:]) >= firstHalfRevNum:
+                ans -= 1
+        elif int(S[halfLen:]) >= firstHalfRevNum:
+                ans -= 1
+        return ans
