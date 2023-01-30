63448a73e0a9b8c39810dcec8ba46d060ac656cb
John <zathrath03@gmail.com>
2023-01-29 19:29:00 -0800

    Time: 269 ms (68.75%), Space: 14.2 MB (75.00%) - LeetHub

--- 1908-game-of-nim/1908-game-of-nim.py
+++ 1908-game-of-nim/1908-game-of-nim.py
@@ -1,21 +1,26 @@
 class Solution:
     def nimGame(self, piles: List[int]) -> bool:
+        remaining = sum(piles)
         memo = {}
-        mask = 0
-        for v in piles:
-            mask = (mask << 3) + v
 
-        @cache
-        def play(mask):
-            for i in range(len(piles)):
-                t = (mask >> (3 * i)) & 7
-                if not t:
-                    continue
-                for k in range(1, t + 1):
-                    a = k << (3 * i)
-                    if not mask - a or not play(mask - a):
-                        return True
+        return self.__is_next_person_winner(piles, remaining, memo)
+
+    def __is_next_person_winner(self, piles, remaining, memo):
+        key = "-".join(map(str, piles))
+        if key in memo:
+            return memo[key]
+
+        if remaining == 0:
             return False
 
-        return play(mask)
- 
+        for i in range(len(piles)):
+            for j in range(1, piles[i] + 1):
+                piles[i] -= j
+                next_state = sorted(piles)
+                if not self.__is_next_person_winner(next_state, remaining, memo):
+                    memo[key] = True
+                    return True
+                piles[i] += j
+
+        memo[key] = False
+        return False
