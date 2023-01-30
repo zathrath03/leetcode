f15794df60ce714621c594cb436fa104d98652fd
John <zathrath03@gmail.com>
2023-01-30 13:09:52 -0800

    Time: 32 ms (71.25%), Space: 13.9 MB (10.15%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,8 +1,6 @@
 class Solution:
     @cache
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return n
         if n < 3:
-            return 1
+            return 1 if n else 0
         return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
