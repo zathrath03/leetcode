4925f951748de1a02ec35e498b127723d38ba497
John <zathrath03@gmail.com>
2023-02-04 10:53:07 -0800

    Time: 59 ms (10.45%), Space: 14 MB (42.71%) - LeetHub

--- 0734-sentence-similarity/0734-sentence-similarity.py
+++ 0734-sentence-similarity/0734-sentence-similarity.py
@@ -1,17 +1,12 @@
 class Solution:
-    def areSentencesSimilar(self, sentence1, sentence2, similarPairs):
-        if len(sentence1) != len(sentence2):
-            return False
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        def are_similar(word1, word2):
+            return word1 == word2 or (word1, word2) in similar or (word2, word1) in similar
+        
+        similar = set([(word1, word2) for (word1, word2) in similarPairs])
 
-        wordToSimilarWords = defaultdict(set)
-
-        for word1, word2 in similarPairs:
-            wordToSimilarWords[word1].add(word2)
-            wordToSimilarWords[word2].add(word1)
-
-        for i in range(len(sentence1)):
-            if sentence1[i] == sentence2[i] or sentence2[i] in wordToSimilarWords[sentence1[i]]:
-                continue
+        for word1, word2 in zip_longest(sentence1, sentence2):
+            if are_similar(word1, word2): continue
             return False
 
         return True
